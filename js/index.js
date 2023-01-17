afb8a6729c4ef2a4a2f384fe23b7893cf930b9f6
KANNAPE <bergerthomas38@gmail.com>
2023-01-18 00:22:07 +0100

    continue adding content to the portfolio page, temporarily removed the image on the about-me section

--- js/index.js
+++ js/index.js
@@ -1,21 +1,21 @@
 const navToggle = document.querySelector('.nav-toggle');
 const navLinks = document.querySelectorAll('.nav__link');
 
-let backToTop = document.getElementById("back-to-top");
-backToTop.addEventListener('click', function() {
-    document.body.scrollTop = 0;
-    document.documentElement.scrollTop = 0;
-});
+// let backToTop = document.getElementById("back-to-top");
+// backToTop.addEventListener('click', function() {
+//     document.body.scrollTop = 0;
+//     document.documentElement.scrollTop = 0;
+// });
 
-window.onscroll = function() { scrollFunction() };
+// window.onscroll = function() { scrollFunction() };
 
-function scrollFunction() {
-    if (document.body.scrollTop > visualViewport.height * .9 || document.documentElement.scrollTop > visualViewport.height * .9) {
-        backToTop.classList.remove("back-to-top__hidden");
-    } else {
-        backToTop.classList.add("back-to-top__hidden");
-    }
-}
+// function scrollFunction() {
+//     if (document.body.scrollTop > visualViewport.height * .9 || document.documentElement.scrollTop > visualViewport.height * .9) {
+//         backToTop.classList.remove("back-to-top__hidden");
+//     } else {
+//         backToTop.classList.add("back-to-top__hidden");
+//     }
+// }
 
 navToggle.addEventListener('click', () => {
     document.body.classList.toggle('nav-open');
